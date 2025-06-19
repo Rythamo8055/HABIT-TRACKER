1af48fa5e84f6946bcc52c0cf2ad566110110450
Vishnu <mailforservices8055@gmail.com>
2025-06-19 21:58:13 +0000

    still seing that

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import { Header } from '@/components/layout/Header';
 import { BottomNavBar } from '@/components/layout/BottomNavBar';
+import { cn } from '@/lib/utils';
 
 function AppLayoutContent({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 overflow-y-auto p-4 pb-[calc(6rem+env(safe-area-inset-bottom))] md:p-6 lg:p-8">
+      <main className={cn(
+        "flex-1 overflow-y-auto p-4 pb-[calc(6rem+env(safe-area-inset-bottom))] md:p-6 lg:p-8",
+        "no-scrollbar"
+      )}>
         {children}
       </main>
       <BottomNavBar />
